bed66946f6d528dc2747f7bb64e1b0cb31aebbec
John Clapper <59069860+britzky@users.noreply.github.com>
2023-10-22 14:00:49 -0400

    Time: 25 ms (89.55%), Space: 15.1 MB (24.52%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,27 +5,21 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        # check if the strings are the same length
+        char_map = {}
         if len(s) != len(t):
             return False
-        
-        # initialize a hashmap to count the letters
-        # loop through s and count each letter in the hashmap
-        char_count = {}
         for i in range(len(s)):
-            if s[i] not in char_count:
-                char_count[s[i]] = 1
-            else:
-                char_count[s[i]] += 1
-            # if the index of t is in char_count subtract 1 
-            if t[i] not in char_count:
-                char_count[t[i]] = -1
-            else:
-                char_count[t[i]] -= 1
-        #return True if the values of char_count = 0 and False otherwise
-        for value in char_count.values():
-            if value != 0:
-                return False
-        return True      
+            if s[i] in char_map:
+                char_map[s[i]] += 1
+            else: 
+                char_map[s[i]] = 1
+            if t[i] in char_map:
+                char_map[t[i]] -= 1
+            else: 
+                char_map[t[i]] = -1
+        values = char_map.values()
 
-        
+        for i in range(len(values)):
+            if values[i] != 0:
+                return False
+        return True

a7dd8e543f0266cbdc635e5105e3c023107e0068
John Clapper <59069860+britzky@users.noreply.github.com>
2023-10-22 12:46:22 -0400

    Time: 131 ms (13.94%), Space: 16.4 MB (87.87%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,30 +1,24 @@
 class Solution(object):
     def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        # initialize a count hashmap 
-        # make an empty list the length of nums
-        # loop through nums and count each element
-        n = len(nums)
-        frequency = [[] for _ in range(n + 1)]
-        count = {}
-        result = []
+        m = {}
+        r = []
+        for n in nums:
+            if n in m:
+                m[n] += 1
+            else:
+                m[n] = 1
 
-        for num in nums:
-            count[num] = count.get(num, 0) + 1 
+        for j in range(k):
+            max_freq = 0
+            max_freq_num = 0
+            for key, value in m.items():
+                if value > max_freq:
+                    max_freq = value
+                    max_freq_num = key
+            r.append(max_freq_num)
+            m.pop(max_freq_num)
 
-        for key, value in count.items():
-            frequency[value].append(key)
-
-        for i in range(n, 0, -1):
-            for num in frequency[i]:
-                result.append(num)
-                if len(result) == k:
-                    return result
-
+        return r
     
 
 

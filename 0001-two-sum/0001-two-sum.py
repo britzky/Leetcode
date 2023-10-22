22dd68be25cc30adc8e84ac71743fe5f729c6bbc
John Clapper <59069860+britzky@users.noreply.github.com>
2023-10-22 15:39:58 -0400

    Time: 29 ms (97.61%), Space: 14 MB (91.39%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,23 +5,12 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        # initalize a result list
-        # initialize a hashmap 
-        # loop through nums
-        # initialize a variable for the needed_value which would be target - current index value
-        # check if the needed_value is in the hashmap
-        # if it is the needed value then append the key of the number from the dictionary 
-        # append the current index to the result list
-        # if it isn't the needed value then add the index as the key and the number as the value
-        # return the result list
-
-        result = []
         hash_map = {}
         for i in range(len(nums)):
-            needed_value = target - nums[i]    
-            if needed_value in hash_map:
-                result.append(hash_map[needed_value])
-                result.append(i)
+            compliment = target - nums[i]
+            if compliment in hash_map:
+                return [hash_map[compliment], i]
             else:
                 hash_map[nums[i]] = i
-        return result
+
+

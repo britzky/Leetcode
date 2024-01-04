e6309be3fc1d60b4d92a7ba13a1645fbb41436be
John Clapper <59069860+britzky@users.noreply.github.com>
2024-01-04 13:59:35 -0700

    Time: 88 ms (89.92%), Space: 20.9 MB (39.28%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,19 +1,7 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        # initalize a result hashmap as a default dict that holds lists as the value
-        # loop through strs
-        # initlialize a count array filled with 26 0's
-        # loop through the chars of strs
-        # insert the order of the current char - the order of 'a' into the count array
-        result_map = defaultdict(list)
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
         for string in strs:
-            count = [0] * 26
-            for char in string:
-                count[ord(char) - ord('a')] += 1
-            result_map[tuple(count)].append(string)
-        return result_map.values()
-        
+            sorted_string = ''.join(sorted(string))
+            anagrams[sorted_string].append(string)
+        return anagrams.values()

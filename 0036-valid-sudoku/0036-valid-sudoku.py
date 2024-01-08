6d6f95534049a6e65b1b313648999d31bb5e924d
John Clapper <59069860+britzky@users.noreply.github.com>
2024-01-08 14:06:53 -0700

    Time: 91 ms (88.64%), Space: 17.3 MB (19.07%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,19 +1,17 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = defaultdict(set)
-        cols = defaultdict(set)
+        rows = defaultdict(set)        
+        cols = defaultdict(set)        
         squares = defaultdict(set)
-
         for row in range(9):
             for col in range(9):
-                if board[row][col] == ".":
+                if board[row][col] == '.':
                     continue
                 if (board[row][col] in rows[row] or
                     board[row][col] in cols[col] or
-                    board[row][col] in squares[(row//3,col//3)]):
+                    board[row][col] in squares[(row//3, col//3)]):
                     return False
                 rows[row].add(board[row][col])
                 cols[col].add(board[row][col])
-                squares[(row//3,col//3)].add(board[row][col])
+                squares[(row//3, col//3)].add(board[row][col])
         return True
-

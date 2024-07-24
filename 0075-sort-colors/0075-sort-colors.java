3fba1f174e76fc6604dbb5e9795ac812d5758c22
John Clapper <johnclapper89@gmail.com>
2024-07-24 08:43:36 -0400

    Time: 0 ms (100%), Space: 41.9 MB (24.67%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int mid = 0;
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (mid <= right) {
+            if (nums[mid] == 0) {
+                swap(nums, mid++, left++);
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else if (nums[mid] == 2) {
+                swap(nums, mid, right--);
+            }
+        }
+    }
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}

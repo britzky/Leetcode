74f144dd68f3230b63c3c1c83cabc5db10044eb5
John Clapper <johnclapper89@gmail.com>
2024-08-01 08:43:03 -0400

    Time: 31 ms (26.1%), Space: 63.4 MB (5.15%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,51 +1,22 @@
-class Solution {
+public class Solution {
     public int[] sortArray(int[] nums) {
-        return helper(nums, 0, nums.length - 1);
-    }
+        List<Integer>[] buckets = new ArrayList[100001];
 
-    public int[] helper(int[] nums, int s, int e) {
-        if (e - s + 1 <= 1) {
-            return nums;
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i] = new ArrayList<>();
         }
 
-        int m = (s + e) / 2;
+        for (int num : nums) {
+            int i = num + 50000;
+            buckets[i].add(num);
+        }
 
-        helper(nums, s, m);
-        helper(nums, m + 1, e);
-        merge(nums, s, m, e);
-
-        return nums;
-    }
-
-    public void merge(int[] nums, int s, int m, int e){
-        int[] L = Arrays.copyOfRange(nums, s, m + 1);
-        int[] R = Arrays.copyOfRange(nums, m + 1, e + 1);
-
-        int i = 0;
-        int j = 0;
-        int k = s;
-
-        while (i < L.length && j < R.length) {
-            if (L[i] <= R[j]){
-                nums[k] = L[i];
-                i++;
-            } else {
-                nums[k] = R[j];
-                j++;
+        int index = 0;
+        for (int i = 0; i < buckets.length; i++) {
+            for (int num : buckets[i]) {
+                nums[index++] = num;
             }
-            k++;
         }
-
-        while (i < L.length) {
-            nums[k] = L[i];
-            i++;
-            k++;
-        }
-
-        while (j < R.length) {
-            nums[k] = R[j];
-            j++;
-            k++;
-        }
+        return nums;
     }
 }

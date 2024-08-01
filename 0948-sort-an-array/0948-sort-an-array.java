53c10a8106f4c12d03a28580badce55a23df8c20
John Clapper <johnclapper89@gmail.com>
2024-08-01 08:31:58 -0400

    Time: 27 ms (47.46%), Space: 56 MB (58.4%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,20 +1,23 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        return sortHelper(nums, 0, nums.length - 1);
+        return helper(nums, 0, nums.length - 1);
     }
 
-    public int[] sortHelper(int[] nums, int s, int e) {
+    public int[] helper(int[] nums, int s, int e) {
         if (e - s + 1 <= 1) {
             return nums;
         }
+
         int m = (s + e) / 2;
-        sortHelper(nums, s, m);
-        sortHelper(nums, m + 1, e);
+
+        helper(nums, s, m);
+        helper(nums, m + 1, e);
         merge(nums, s, m, e);
+
         return nums;
     }
 
-    public void merge(int[] nums, int s, int m, int e) {
+    public void merge(int[] nums, int s, int m, int e){
         int[] L = Arrays.copyOfRange(nums, s, m + 1);
         int[] R = Arrays.copyOfRange(nums, m + 1, e + 1);
 
@@ -23,7 +26,7 @@ class Solution {
         int k = s;
 
         while (i < L.length && j < R.length) {
-            if (L[i] <= R[j]) {
+            if (L[i] <= R[j]){
                 nums[k] = L[i];
                 i++;
             } else {

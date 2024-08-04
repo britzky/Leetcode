e901b61386195d0ad3f46e4699e97d87077c9d32
John Clapper <johnclapper89@gmail.com>
2024-08-04 08:22:41 -0400

    Time: 31 ms (26.08%), Space: 63 MB (5.2%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,30 +1,24 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        //bucket sort
-        
-        //Initialize buckets
         List<Integer>[] buckets = new ArrayList[100001];
+
         for(int i = 0; i < buckets.length; i++) {
             buckets[i] = new ArrayList<>();
         }
 
-        //Fill the buckets
         for (int num : nums) {
             int i = num + 50000;
             buckets[i].add(num);
         }
 
-        //Empty the buckets back into nums
         int index = 0;
-        for (int i = 0; i < buckets.length; i++) {
+        for (int i = 0; i < buckets.length; i++){
             for (int num : buckets[i]) {
                 nums[index] = num;
                 index++;
             }
-        }   
+        }
 
-        //return the sorted array
         return nums;
-
     }
 }

83dfa5b1157e83a146f19a144895da0843963505
John Clapper <johnclapper89@gmail.com>
2024-08-10 08:01:59 -0400

    Time: 26 ms (54.26%), Space: 55.8 MB (61.89%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,21 +1,23 @@
 class Solution {
     public int[] sortArray(int[] nums) {
         //merge sort
-        return helper(nums, 0, nums.length -1);
+        return helper(nums, 0, nums.length - 1);
     }
 
     private int[] helper(int[] nums, int s, int e) {
         if (e - s + 1 <= 1) {
             return nums;
-        }        
-        int m = (s + e) / 2;
+        }
         
+        int m = (s + e) / 2;
+
         helper(nums, s, m);
         helper(nums, m + 1, e);
+        
         merge(nums, s, m, e);
+        
         return nums;
     }
-
     private void merge(int[] nums, int s, int m, int e) {
         int[] L = Arrays.copyOfRange(nums, s, m + 1);
         int[] R = Arrays.copyOfRange(nums, m + 1, e + 1);
@@ -25,18 +27,17 @@ class Solution {
         int k = s;
 
         while (i < L.length && j < R.length) {
-            if (L[i] < R[j]){
+            if (L[i] <= R[j]) {
                 nums[k++] = L[i++];
             } else {
                 nums[k++] = R[j++];
             }
         }
-
+        
         while (i < L.length) {
             nums[k++] = L[i++];
         }
-
-        while (j < R.length) {
+        while(j < R.length) {
             nums[k++] = R[j++];
         }
     }

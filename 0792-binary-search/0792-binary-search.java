dc0dc915c7a6818f4374b32e6e8ab0cb11789bb7
John Clapper <johnclapper89@gmail.com>
2024-08-11 06:40:28 -0400

    Time: 0 ms (100%), Space: 45.8 MB (28.81%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -3,7 +3,7 @@ class Solution {
         int l = 0;
         int r = nums.length - 1;
 
-        while(l <= r) {
+        while (l <= r) {
             int m = l + (r - l) / 2;
             if (nums[m] > target) {
                 r = m - 1;

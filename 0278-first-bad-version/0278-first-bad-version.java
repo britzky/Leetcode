d77a5b658c789e5d3cb3bece9b552b0ee4f54a37
John Clapper <johnclapper89@gmail.com>
2024-08-11 15:43:12 -0400

    Time: 25 ms (67.97%), Space: 40.1 MB (77.12%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -4,9 +4,10 @@
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int l = 1;
-        int r = n;
+        int r = n; 
         int badVersion = 0;
-        while(l <= r) {
+
+        while (l <= r) {
             int m = l + (r - l) / 2;
             if (isBadVersion(m)) {
                 badVersion = m;

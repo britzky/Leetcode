8fa03eae9ab5f7fd813f8307376955ee34225241
John Clapper <johnclapper89@gmail.com>
2024-08-11 07:30:48 -0400

    Time: 28 ms (17.37%), Space: 45.2 MB (22.78%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int l = 1;
+        int r = 1;
+
+        for (int pile : piles) {
+            r = Math.max(pile, r);
+        }
+
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            int hoursSpent = 0;
+
+            for (int pile : piles) {
+                hoursSpent += Math.ceil((double) pile / m);
+            }
+
+            if (hoursSpent <= h) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+
+        }
+
+        return r;
+    }
+}

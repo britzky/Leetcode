3d1c2ee1b4a0a20f903055c964a3273af57a5686
John Clapper <johnclapper89@gmail.com>
2024-08-21 07:28:14 -0400

    Time: 0 ms (100%), Space: 41.9 MB (8.2%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -19,10 +19,11 @@ class Solution {
         dfs(root, res);
         return res;
     }
+    
     private void dfs(TreeNode root, List<Integer> res) {
         if (root == null) return;
         dfs(root.left, res);
         res.add(root.val);
         dfs(root.right, res);
     }
-}
+}   
